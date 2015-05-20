82a68de96b2fa4bcfd4845e41c8753ec3dc7d36a
fdesimini <fdesimini@users.noreply.github.com>
2015-05-20 15:58:38 -0400

    added background

--- SlimPickensGame/GameScene.swift
+++ SlimPickensGame/GameScene.swift
@@ -8,34 +8,58 @@
 
 import SpriteKit
 
+
+// create sprite nodes
+
 var slimPickens = SKSpriteNode()
+var bg = SKSpriteNode()
 
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-//        let myLabel = SKLabelNode(fontNamed:"Euphemia UCAS")
-//        myLabel.text = "Yee Haw!";
-//        myLabel.fontSize = 65;
-//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-//        
-//        self.addChild(myLabel)
-        
-        self.backgroundColor = UIColor.magentaColor()
-        
+       
+        self.backgroundColor = UIColor.greenColor()
+
         var slimTexture = SKTexture(imageNamed: "nucular_bird1")
         var slimTexture2 = SKTexture(imageNamed: "nucular_bird2")
         var slimTexture3 = SKTexture(imageNamed: "nucular_bird3")
         var slimTexture4 = SKTexture(imageNamed: "nucular_bird4")
+ 
+        // create variable for animation and create action (SKAction)
+        
+        var animation = SKAction.animateWithTextures([slimTexture, slimTexture2, slimTexture3, slimTexture4], timePerFrame: 0.15)
+        var makeSlimFlap = SKAction.repeatActionForever(animation)
+        
+        
+// Add/assign texture to the node
         
         slimPickens = SKSpriteNode(texture: slimTexture)
         slimPickens.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
         
-        var animation = SKAction.animateWithTextures([slimTexture, slimTexture2, slimTexture3, slimTexture4], timePerFrame: 0.15)
-        var makeSlimFlap = SKAction.repeatActionForever(animation)
+// attachd the animation to the node
         slimPickens.runAction(makeSlimFlap)
         
         self.addChild(slimPickens)
+        
+//        add a background
+        
+        //define ground object
+        var ground = SKNode()
+        //set the ground position
+        ground.position = CGPointMake(0, 0)
+        //add physics
+        ground.physicsBody = SKPhysicsBody (rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
+        ground.physicsBody?.dynamic = false
+        
+        self.addChild(ground)
+        
+        //add the background to the scene
+        var backgroundImage1 = SKTexture(imageNamed: "bg_depthBlurred_front")
+        bg = SKSpriteNode(texture: backgroundImage1)
+        bg.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMinY(self.frame))
+        bg.size.height = 504
+        self.addChild(bg)
         
     }
     
@@ -45,17 +69,17 @@ class GameScene: SKScene {
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+//            let sprite = SKSpriteNode(imageNamed:"nucular_bird1")
+//            
+//            sprite.xScale = 0.5
+//            sprite.yScale = 0.5
+//            sprite.position = location
+//            
+//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+//            
+//            sprite.runAction(SKAction.repeatActionForever(action))
+//            
+//            self.addChild(sprite)
         }
     }
    
